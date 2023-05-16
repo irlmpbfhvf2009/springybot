214012cec9d17ec7cb6f6e5ea84cd6eb8bbf9ccd
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2023-05-16 11:00:57 +0800

    fix

--- src/main/java/com/lwdevelop/bot/coolbao/handler/messageEvent/private_/commands/cgBalance.java
+++ src/main/java/com/lwdevelop/bot/coolbao/handler/messageEvent/private_/commands/cgBalance.java
@@ -14,12 +14,15 @@ import com.lwdevelop.bot.coolbao.utils.Common;
 public class cgBalance {
 
     private final static String xxpay_account_url = "http://4pay.ddb22.vip/xxpay-manage/api/account/get";
+    private final static String xxpay_login_url = "http://4pay.ddb22.vip/xxpay-manage/api/auth";
 
     public String cmd(Common common) {
-        String cg01_params = "mchId=20000007&access_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsZW8iLCJjcmVhdGVkIjoxNjgzNTUwNzQ1NTA1LCJpZCI6MiwiZXhwIjoxNjg0MTU1NTQ1fQ.pFINl0q6yHpykONOaK4kpDeBfG6dz1pxPz367JMLPfM64UN47n1-bFLZzPazNBhpcAfbB0JhLe_27L452UXtwA";
-        String cg02_params = "mchId=20000008&access_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsZW8iLCJjcmVhdGVkIjoxNjgzNTUwNzQ1NTA1LCJpZCI6MiwiZXhwIjoxNjg0MTU1NTQ1fQ.pFINl0q6yHpykONOaK4kpDeBfG6dz1pxPz367JMLPfM64UN47n1-bFLZzPazNBhpcAfbB0JhLe_27L452UXtwA";
-        String cg03_params = "mchId=20000009&access_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsZW8iLCJjcmVhdGVkIjoxNjgzNTUwNzQ1NTA1LCJpZCI6MiwiZXhwIjoxNjg0MTU1NTQ1fQ.pFINl0q6yHpykONOaK4kpDeBfG6dz1pxPz367JMLPfM64UN47n1-bFLZzPazNBhpcAfbB0JhLe_27L452UXtwA";
-        String cg04_params = "mchId=20000010&access_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsZW8iLCJjcmVhdGVkIjoxNjgzNTUwNzQ1NTA1LCJpZCI6MiwiZXhwIjoxNjg0MTU1NTQ1fQ.pFINl0q6yHpykONOaK4kpDeBfG6dz1pxPz367JMLPfM64UN47n1-bFLZzPazNBhpcAfbB0JhLe_27L452UXtwA";
+
+        String token = generateToken();
+        String cg01_params = "mchId=20000007&access_token="+token;
+        String cg02_params = "mchId=20000008&access_token="+token;
+        String cg03_params = "mchId=20000009&access_token="+token;
+        String cg04_params = "mchId=20000010&access_token="+token;
 
         String cg01 = sendGetRequest(xxpay_account_url, cg01_params);
         String cg02 = sendGetRequest(xxpay_account_url, cg02_params);
@@ -72,6 +75,61 @@ public class cgBalance {
                     JsonNode data = root.path("data").path("balance");
                     ;
                     return data.asText();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                throw new IOException("GET request failed with response code: " + responseCode);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String generateToken() {
+        String params = "username=leo&password=as794613";
+        String response = sendGetRequest(xxpay_login_url, params, "access_token");
+        return response;
+    }
+
+    
+    private static String sendGetRequest(String urlString, String params, String type) {
+        String fullUrl = urlString + "?" + params;
+        try {
+            URL url = new URL(fullUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                StringBuilder response = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                reader.close();
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+                    JsonNode root = mapper.readTree(response.toString());
+                    JsonNode data;
+                    switch (type) {
+                        case "access_token":
+                            data = root.path("data").path("access_token");
+                            return data.asText();
+                        case "mchId":
+                            data = root.path("data").path("mchId");
+                            return data.asText();
+                        case "name":
+                            data = root.path("data").path("name");
+                            return data.asText();
+                        case "mobile":
+                            data = root.path("data").path("mobile");
+                            return data.asText();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
