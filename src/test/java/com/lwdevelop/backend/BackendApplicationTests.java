a309bec4119dbc430a2d714fd80ecd512da0ecb8
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2023-07-20 10:47:59 +0800

    -

--- src/test/java/com/lwdevelop/backend/BackendApplicationTests.java
+++ src/test/java/com/lwdevelop/backend/BackendApplicationTests.java
@@ -1,19 +1,11 @@
 package com.lwdevelop.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.lwdevelop.repository.SpringyBotRepository;
-import com.lwdevelop.utils.RedisUtils;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BackendApplicationTests {
 
-    @Autowired
-    SpringyBotRepository springyBotRepository;
-
-    @Autowired
-    RedisUtils redisUtils;
 
     @Test
     public void test() {
